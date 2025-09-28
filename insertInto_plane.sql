d4901785e40abffa01956147ae9310293abb0a66
Alasdair Wilmot <103636196+Alasdair97@users.noreply.github.com>
2025-09-28 23:53:31 +0000

    Insert plane data

---
+++ insertInto_plane.sql
@@ -0,0 +1,49 @@
+INSERT INTO planes (tail_number, plane_model_id)
+VALUES
+    ('G-ABCD', 7),
+    ('G-EFGH', 5),
+    ('G-IJKL', 5),
+    ('G-MNOP', 1),
+    ('G-QRST', 1),
+    ('G-UVWX', 1),
+    ('G-YZAB', 1),
+    ('G-CDEF', 1),
+    ('G-GHIJ', 1),
+    ('G-KLMN', 2),
+    ('G-OPQR', 2),
+    ('G-STUV', 2),
+    ('G-WXYZ', 2),
+    ('G-ABEF', 2),
+    ('G-CDGH', 2),
+    ('G-IJLM', 3),
+    ('G-NOPQ', 3),
+    ('G-RSTU', 3),
+    ('G-VWXY', 3),
+    ('G-ZABC', 3),
+    ('G-DEFG', 3),
+    ('G-HIJK', 4),
+    ('G-LMNO', 4),
+    ('G-PQRS', 4),
+    ('G-TUVW', 4),
+    ('G-XYZA', 4),
+    ('G-BCDE', 6),
+    ('G-FGHI', 6),
+    ('G-JKLM', 8),
+    ('G-NOPR', 8),
+    ('G-QRST', 8),
+    ('G-UVXY', 8),
+    ('G-ZABD', 9),
+    ('G-CDFE', 9),
+    ('G-GHIU', 10),
+    ('G-JKLO', 10),
+    ('G-MNPQ', 10),
+    ('G-RSTI', 10),
+    ('G-VWXK', 10),
+    ('G-YZAE', 10);
+
+/* 
+    This was generated using ChatGPT as per exceptions in the worksheet
+    After giving it an example for the first plane
+    I then asked for a selection of tailnumbers stating the range of model_ids
+    I also specifed the tailnumbers would start with G and that there would be very few of specifed plane ids
+*/
