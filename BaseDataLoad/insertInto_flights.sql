35376e9c10912e085c5111ea56b4468eb0847899
Alasdair Wilmot <103636196+Alasdair97@users.noreply.github.com>
2025-09-29 02:12:32 +0000

    Base Flight

---
+++ BaseDataLoad/insertInto_flights.sql
@@ -0,0 +1,3 @@
+INSERT INTO 'flights' ('origin_location_id','destination_location_id','flight_pilot_id','flight_plane_id','departure_time_utc','arrival_time_utc','passengers_booked','flight_status','time_delay')
+VALUES
+    (1,21,74,35,'31/10/25 12:00:00.000000','31/10/25 12:00:00.000000',68,'On Time', NULL);
--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class DBOperations:
       self.conn.close()
 
   def insert_base_data(self):
-    tables = ['locations','aircraftTypeRating','aircraftModel','plane','pilot']
+    tables = ['locations','aircraftTypeRating','aircraftModel','plane','pilot','flights']
     try:
       self.get_connection()
       for table in tables:
