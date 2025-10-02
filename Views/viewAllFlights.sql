adab0ebe24904168149aedfdd4a07e992428e7a9
Alasdair Wilmot <103636196+Alasdair97@users.noreply.github.com>
2025-10-02 10:24:23 +0000

    Expaned VIEW

--- Views/viewAllFlights.sql
+++ Views/viewAllFlights.sql
@@ -5,7 +5,17 @@ SELECT
     d.iata_code AS Destination,
     STRFTIME('%R',f.departure_time_utc) AS Schedualed,
     STRFTIME('%R',f.departure_time_utc,f.time_delay) AS Expected,
-    f.flight_status AS Status
+    f.flight_status AS Status,
+    o.city AS 'Origin City',
+    d.city AS 'Destination City',
+    o.icao_code AS 'Origin Alt Code',
+    d.icao_code AS 'Destination Alt Code',
+    o.location_name AS 'Origin Airport',
+    d.location_name AS 'Destination Airport',
+    p.tailnumber AS Tailnumber,
+    m.aircraft_model_name AS Aircraft
     from flights f
     JOIN locations o on f.origin_location_id = o.location_id
-    JOIN locations d on f.destination_location_id = d.location_id; 
+    JOIN locations d on f.destination_location_id = d.location_id
+    JOIN plane p on f.flight_plane_id = p.plane_id
+    JOIN aircraftModel m on p.plane_model_id = m.aircraft_model_id;
